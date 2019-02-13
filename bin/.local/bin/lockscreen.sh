102f5ef18843b58c7fb11f0bcbd95a3ba1901e07
Per Hellström <per.hellstrom@dewire.com>
2019-02-13 08:50:51 +0100

    Change to i3lock

--- bin/.local/bin/lockscreen.sh
+++ bin/.local/bin/lockscreen.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
-#scrot /tmp/screenshot.png
-#convert /tmp/screenshot.png -filter Gaussian -resize 20% -define filter:sigma=2.5 -resize 500% /tmp/screenshotblur.png
-#convert -resize 10% /tmp/screenshot.png /tmp/screenshot.png
-#convert -resize 1000% /tmp/screenshot.png /tmp/screenshot.png
-#i3lock -i /tmp/screenshotblur.png
-i3lock -i '/home/per/Pictures/lockscreen.png' -c 000000
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+D='#ffffff22'  # clear ish
+T='#ffffffcc'  # clear ish
+W='#880000bb'  # wrong
+V='#ffffff55'  # verifying
+
+i3lock -i '/home/per/Pictures/lockscreen.png' -c '#000000' \
+--insidevercolor=$C   \
+--ringvercolor=$V     \
+\
+--insidewrongcolor=$C \
+--ringwrongcolor=$W   \
+\
+--insidecolor=$B      \
+--ringcolor=$D        \
+--linecolor=$B        \
+--separatorcolor=$D   \
+\
+--verifcolor=$T        \
+--wrongcolor=$T        \
+--timecolor=$T        \
+--datecolor=$T        \
+--layoutcolor=$T      \
+--keyhlcolor=$T       \
+--bshlcolor=$T        \
+\
+--screen 1            \
+--clock               \
+--indicator           \
+--timestr="%H:%M:%S"  \
+--datestr="%Y-%m-%d" \
