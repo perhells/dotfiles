19e7019ea05d75c7b07be9a04ba7dba96e9dfc05
Per Hellström <per.hellstrom@dewire.com>
2019-02-13 08:50:29 +0100

    Make script more versatile

--- bin/.local/bin/autoxrandr.sh
+++ bin/.local/bin/autoxrandr.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 outputs=$(xrandr | grep '\Wconnected' | awk '{ print $1 }')
+off_outputs=$(xrandr | grep '\Wdisconnected' | awk '{ print $1 }')
 outputcount=$(echo "$outputs" | wc -l)
-echo "$outputs"
-echo "$outputcount"
 
 for output in $outputs; do
-    if [[ $output =~ ^LVDS.*$ ]]; then
-        lvds=$output
+    if [[ $output =~ ^LVDS.*$ ]] || [[ $output =~ ^eDP.*$ ]]; then
+        main=$output
+        break
     fi
 done
 
 if [ "$outputcount" -gt 1 ]; then
+    echo "$main: auto primary"
+    xrandr --output $main --primary --auto
+    previous=$main
     for output in $outputs; do
-        if [[ ! $output =~ ^LVDS.*$ ]]; then
-            echo xrandr --output $lvds --off --output $output --auto --primary
-            xrandr --output $lvds --off --output $output --auto --primary
-            exit 0
+        if [[ ! $output =~ ^LVDS.*$ ]] && [[ ! $output =~ ^eDP.*$ ]]; then
+            echo "xrandr $output: auto (above $previous)"
+            xrandr --output $output --auto --above $previous
+            previous=$output
         fi
     done
+    for output in $off_outputs; do
+        echo "$output: off"
+        xrandr --output $output --off
+    done
 else
-    if [ "$lvds" != "" ]; then
-        echo xrandr --output $lvds --auto --primary
-        xrandr --output $lvds --auto --primary
-        exit 0
+    if [ "$main" != "" ]; then
+        echo "$main: auto primary"
+        xrandr --output $main --auto --primary
+        for output in $off_outputs; do
+            echo "$output: off"
+            xrandr --output $output --off
+        done
     else
-        echo "Couldn't find any output starting with \"LVDS\"!"
+        echo "Couldn't find any output starting with \"LVDS\" or eDP!"
         exit 1
     fi
 fi
