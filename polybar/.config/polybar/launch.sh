416ec974475caf4c4c1f17b5bc5af81abb78abed
Per Hellström <per.hellstrom@dewire.com>
2019-09-23 12:56:02 +0200

    Add basic bar selection

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,6 +7,19 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar xps &
+HOSTNAME=$(hostname)
+if [ "$HOSTNAME" == "caesium" ]; then
+  bar=4k
+else
+  bar=default
+fi
+
+if type "xrandr"; then
+  for MONITOR in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$MONITOR polybar --reload $bar &
+  done
+else
+  polybar --reload $bar &
+fi
 
 echo "Polybar launched..."
