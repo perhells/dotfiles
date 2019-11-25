b8469cfbe58035e195540ebf0384cfaab6065d7b
Per Hellström <per.hellstrom@dewire.com>
2019-11-25 10:13:42 +0100

    Use variables in polybar config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ "$#" -eq 1 ]; then
+  DPI=$1
+else
+  if [ "$HOSTNAME" == "caesium" ]; then
+    DPI=192
+  else
+    DPI=96
+  fi
+fi
+
+let "HEIGHT = $DPI / 4"
+let "TRAYSIZE = $DPI / 6"
+let "LINESIZE = $DPI / 32"
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -17,12 +31,18 @@ fi
 if type "xrandr" > /dev/null; then
   TRAY_POS=right
   for MONITOR in $(xrandr --query | grep " connected" | sort | cut -d" " -f1); do
-    echo "Starting polybar $bar on $MONITOR"
-    MONITOR=$MONITOR TRAY_POS=$TRAY_POS polybar --reload $bar &
+    echo "MONITOR=$MONITOR DPI=$DPI TRAY_POS=$TRAY_POS polybar --reload custom &"
+    MONITOR=$MONITOR \
+      DPI=$DPI \
+      HEIGHT=$HEIGHT \
+      TRAYSIZE=$TRAYSIZE \
+      LINESIZE=$LINESIZE \
+      TRAY_POS=$TRAY_POS \
+      polybar --reload custom &
     TRAY_POS=""
   done
 else
   polybar --reload $bar &
 fi
 
-echo "Polybar launched..."
+echo "Polybars launched..."
