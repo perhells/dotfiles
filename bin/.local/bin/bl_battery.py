878e324990624386c29a7e19af39c6681f7764a3
Per Hellström <per.hellstrom@dewire.com>
2020-03-23 08:42:19 +0100

    Add bluetooth battery monitoring

---
+++ bin/.local/bin/bl_battery.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import socket, sys
+
+def send(sock, message):
+    sock.send(b"\r\n" + message + b"\r\n")
+
+def getATCommand(sock, line):
+    if b"BRSF" in line:
+        send(sock, b"+BRSF:20")
+        send(sock, b"OK")
+    elif b"CIND=" in line:
+        send(sock, b"+CIND: (\"battchg\",(0-5))")
+        send(sock, b"OK")
+    elif b"CIND?" in line:
+        send(sock, b"+CIND: 5")
+        send(sock, b"OK")
+    elif b"IPHONEACCEV" in line:
+        if not b',' in line:
+            return True
+
+        parts = line[line.index(b',') + 1 : -1].split(b',')
+        if len(parts) < 1 or (len(parts) % 2) != 0:
+            return True
+
+        i = 0
+        while i < len(parts):
+            key = int(parts[i])
+            val = int(parts[i + 1])
+
+            if key == 1:
+                blevel = (val + 1) * 10
+                print("{}%".format(blevel))
+                return False
+            i += 2
+    else:
+        send(sock, b"OK")
+    return True
+
+def main():
+    if (len(sys.argv) < 2):
+        print("Usage: bl_battery.py <BT_MAC_ADDRESS> <PORT>")
+        exit()
+
+    BT_ADDRESS = sys.argv[1]
+
+    port = int(sys.argv[2])
+
+    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    s.connect((BT_ADDRESS, port))
+
+    while getATCommand(s, s.recv(128)):
+        pass
+
+if __name__ == "__main__":
+    main()
