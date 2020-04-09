41646bee46c64241542d29f1d6cabfd49110fdd8
Per Hellström <per.hellstrom@dewire.com>
2020-04-09 17:20:38 +0200

    Add DPI argument to mirror polybar

--- polybar/.config/polybar/mirror.sh
+++ polybar/.config/polybar/mirror.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ "$#" -eq 1 ]; then
+  DPI=$1
+else
+  if [ "$HOSTNAME" == "caesium" ]; then
+    DPI=144
+  else
+    DPI=96
+  fi
+fi
+
+let "HEIGHT = $DPI / 4"
+let "TRAYSIZE = $DPI / 6"
+let "LINESIZE = $DPI / 32"
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -8,11 +22,23 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr" > /dev/null; then
   for MONITOR in $(xrandr --query | grep " connected" | sort | cut -d" " -f1); do
-    echo "Starting polybar $bar on $MONITOR"
-    MONITOR=$MONITOR TRAY_POS=right polybar --reload mirror &
+    TRAY_POS=""
+    if xrandr | grep primary | grep $MONITOR > /dev/null; then
+      TRAY_POS="right"
+    fi
+
+    echo "MONITOR=$MONITOR DPI=$DPI TRAY_POS=$TRAY_POS polybar --reload mirror &"
+    MONITOR=$MONITOR \
+      DPI=$DPI \
+      HEIGHT=$HEIGHT \
+      TRAYSIZE=$TRAYSIZE \
+      LINESIZE=$LINESIZE \
+      TRAY_POS=$TRAY_POS \
+      polybar --reload mirror &
   done
 else
-  polybar --reload mirror &
+  echo "Failed to run xrandr..."
+  echo 1
 fi
 
-echo "Polybar launched..."
+echo "Polybars launched..."
