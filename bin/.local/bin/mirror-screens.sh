cdeb7108daa496a328152c6156ea0750dbc94d65
Per Hellström <per.hellstrom@dewire.com>
2020-04-09 17:17:12 +0200

    Add 1080p monitor mirroring

--- bin/.local/bin/mirror-screens.sh
+++ bin/.local/bin/mirror-screens.sh
@@ -13,10 +13,12 @@ else
         echo "Using DVI-I-1 as main output!"
         xrandr --fb 2560x1600 --output DVI-I-1 --auto --scale 1x1 --output HDMI-0 --mode 1920x1080 --scale-from 2560x1600 --same-as DVI-I-1
     else
-        echo "Invalid output: $1"
-        echo "Expected HDMI-0 or DVI-I-1"
-        exit 1
+        if [ $1 == '1080p' ]; then
+            xrandr --output DP-1 --mode 1920x1080 --primary --output HDMI-0 --mode 1920x1080 --same-as DP-1
+        else
+            echo "Invalid output: $1"
+            echo "Expected HDMI-0 or DVI-I-1"
+            exit 1
+        fi
     fi
 fi
-
-~/.config/polybar/mirror.sh
