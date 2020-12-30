828391d9eaea27792305d068834829a8a83867df
Per Hellström <per.hellstrom@knightec.se>
2020-12-30 10:15:09 +0100

    Add interface detection to wlan/eth

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+HOSTNAME=$(hostname)
+
+if [ "$HOSTNAME" == "caesium" ]; then
+  DPI=144
+  bar=4k
+else
+  DPI=96
+  bar=default
+fi
+
 if [ "$#" -eq 1 ]; then
   DPI=$1
-else
-  if [ "$HOSTNAME" == "caesium" ]; then
-    DPI=144
-  else
-    DPI=96
-  fi
 fi
+
+ETH_INTERFACE=$(ifconfig | grep "^e" | awk -F : '{print $1}')
+WLAN_INTERFACE=$(ifconfig | grep "^w" | awk -F : '{print $1}')
 
 let "HEIGHT = $DPI / 4"
 let "TRAYSIZE = $DPI / 6"
@@ -21,29 +28,30 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-HOSTNAME=$(hostname)
-if [ "$HOSTNAME" == "caesium" ]; then
-  bar=4k
-else
-  bar=default
-fi
-
 if type "xrandr" > /dev/null; then
-  #grep " connected"
-  #grep "connected\(\s\w\+\)*[0-9]\+x[0-9]\++[0-9]\++[0-9]\+"
   for MONITOR in $(xrandr --query | grep "connected\(\s\w\+\)*[0-9]\+x[0-9]\++[0-9]\++[0-9]\+" | sort | cut -d" " -f1); do
     TRAY_POS=""
     if xrandr | grep primary | grep $MONITOR > /dev/null; then
       TRAY_POS="right"
     fi
 
-    echo "MONITOR=$MONITOR DPI=$DPI TRAY_POS=$TRAY_POS polybar --reload custom &"
+    echo MONITOR=$MONITOR \
+      DPI=$DPI \
+      HEIGHT=$HEIGHT \
+      TRAYSIZE=$TRAYSIZE \
+      LINESIZE=$LINESIZE \
+      TRAY_POS=$TRAY_POS \
+      ETH_INTERFACE=$ETH_INTERFACE \
+      WLAN_INTERFACE=$WLAN_INTERFACE \
+      polybar --reload custom &
     MONITOR=$MONITOR \
       DPI=$DPI \
       HEIGHT=$HEIGHT \
       TRAYSIZE=$TRAYSIZE \
       LINESIZE=$LINESIZE \
       TRAY_POS=$TRAY_POS \
+      ETH_INTERFACE=$ETH_INTERFACE \
+      WLAN_INTERFACE=$WLAN_INTERFACE \
       polybar --reload custom &
   done
 else
